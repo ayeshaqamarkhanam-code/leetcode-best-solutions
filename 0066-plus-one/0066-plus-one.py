d5ccb4c4c9dfd1f6bad127d9e2a720074c71c495
Ayesha Qamar Khanam <230700614+ayeshaqamarkhanam-code@users.noreply.github.com>
2026-07-15 21:03:37 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,4 +1,3 @@
-'''
 class Solution(object):
     def plusOne(self, digits):
         for i in range(len(digits)-1,-1,-1):
@@ -9,15 +8,3 @@ class Solution(object):
                 return digits
         digits.insert(0,1)
         return digits
-'''
-class Solution:
-    def plusOne(self, digits):
-        n = len(digits)
-
-        for i in range(n - 1, -1, -1):
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-            digits[i] = 0
-
-        return [1] + digits
